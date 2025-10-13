a3085417ce5a85c683cbb488962040018abec88d
PavelGR <grigorevpavelevgenevich@gmail.com>
2025-10-13 08:08:42 +0300

    add seed for random in dataset class

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -11,7 +11,7 @@ class TripletDataset(Dataset):
         self.img_size = img_size
         self.root = Path(root_dir)
         self.transform = transform
-
+        self.random = random.Random(123)
         self.person_dirs = [p for p in self.root.iterdir() if p.is_dir()]
 
         self.images_by_person = {p: list(p.glob("*")) for p in self.person_dirs}
@@ -21,15 +21,15 @@ class TripletDataset(Dataset):
         ]
 
     def __getitem__(self, index):
-        anchor_person = random.choice(self.valid_persons)
+        anchor_person = self.random.choice(self.valid_persons)
         imgs = self.images_by_person[anchor_person]
 
-        a_path, p_path = random.sample(imgs, 2)
+        a_path, p_path = self.random.sample(imgs, 2)
 
-        negative_person = random.choice(
+        negative_person = self.random.choice(
             [p for p in self.person_dirs if p != anchor_person]
         )
-        n_path = random.choice(self.images_by_person[negative_person])
+        n_path = self.random.choice(self.images_by_person[negative_person])
 
         anchor_image = load_image(a_path, self.img_size, self.transform)
         positive_image = load_image(p_path, self.img_size, self.transform)
